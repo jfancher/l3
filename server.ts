4de1813252d10d3e7f2e378ca87d205b813e5f22
Jacob Fancher <jfancher@yext.com>
2021-04-28 14:40:22 -0400

    Support --port flag.

--- server.ts
+++ server.ts
@@ -1,13 +1,18 @@
-import { Call } from "./plugin.ts";
+import { parse } from "https://deno.land/std@0.95.0/flags/mod.ts";
 import { Application, Router } from "https://deno.land/x/oak@v7.3.0/mod.ts";
 
-const args = Deno.args;
-if (args.length === 0 || args.length > 1) {
-  console.error(`usage: ${Deno.mainModule} [plugin-module]`);
+import { Call } from "./plugin.ts";
+
+const args = parse(Deno.args, {
+  default: { port: 8080 },
+});
+
+if (args._.length !== 1) {
+  console.error(`usage: ${Deno.mainModule} [flags] <plugin-module>`);
   Deno.exit(1);
 }
 
-const plugin = await import(args[0]);
+const plugin = await import(args._[0] as string);
 
 const router = new Router();
 router.post("/invoke", async (ctx) => {
@@ -22,4 +27,4 @@ const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen({ port: 8080 });
+await app.listen({ port: args.port });
