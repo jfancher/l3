79ff43b3aa31d7997bf9eac43257886652918346
Jacob Fancher <jfancher@yext.com>
2021-07-12 16:15:27 -0400

    Support loading plugin metadata from a JSON file.

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import { parse } from "https://deno.land/std@0.95.0/flags/mod.ts";
 import { Server } from "./server/mod.ts";
 
 const args = parse(Deno.args, {
-  default: { port: 8080 },
+  default: { config: null, port: 8080 },
 });
 
 if (args._.length !== 1) {
@@ -13,6 +13,11 @@ if (args._.length !== 1) {
 const plugin = {
   module: `file://${Deno.cwd()}/${args._[0]}`,
 };
+
+if (args.config) {
+  const config = JSON.parse(Deno.readTextFileSync(args.config));
+  Object.assign(plugin, config);
+}
 
 const server = new Server(plugin, args.port);
 await server.run();
--- host/plugin.ts
+++ host/plugin.ts
@@ -2,4 +2,10 @@
 export interface Plugin {
   /** The plugin module import path. */
   module: string;
+
+  /** The plugin identifier, if configured. */
+  id?: string;
+
+  /** Additional global values to set for plugin invocations. */
+  globals?: Record<string, unknown>;
 }
